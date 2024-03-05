02fa58c4c4875934d9489750785cf80677d1b410
VinkoRobi2 <161661761+VinkoRobi2@users.noreply.github.com>
2024-03-05 17:14:51 -0500

    Ansley

--- bot.py
+++ bot.py
@@ -15,7 +15,41 @@ device_keyC = '6453e976fc27ea000d816cb1'
 
 token = 'BBUS-1GKmVj02Wr3TCfqEy6LgyyVRgLJVZG'
 
- 
+def analizar_variables(temp,humedad,ruido,contaminacion):
+    temp = int(temp)
+    humedad = int(humedad)
+    ruido = int(ruido)
+    contaminacion = int(contaminacion)
+    
+    mensaje1 = ""
+    mensaje2 = ""
+    mensaje3 = ""
+    mensaje4 = ""
+    mensaje5=  ""
+    mensajeFinal= ""
+    
+    if temp > 30:
+        mensaje1 = "Con temperaturas altas como estas, asegúrate de mantenerte hidratado y usar ropa ligera y fresca. Busca refugio en lugares con aire acondicionado durante las horas más calurosas del día."
+    if humedad > 60:
+        mensaje2 = "Con la humedad elevada, es importante mantenerse fresco y evitar la exposición prolongada al sol. Usa ropa transpirable y busca lugares con ventilación para sentirte más cómodo."
+    if ruido > 60:
+        mensaje3 = "Si el ruido ambiental es alto, considera utilizar tapones para los oídos o buscar lugares tranquilos para relajarte y descansar. El descanso adecuado es clave para mantenerse saludable y enfocado."
+    if contaminacion > 5:
+        mensaje4 = "Cuando la contaminación es alta, intenta limitar tus actividades al aire libre, especialmente si eres sensible a la calidad del aire. Permanece en interiores tanto como sea posible o usa máscaras faciales para reducir la exposición."
+    if temp < 30 and humedad< 60 and ruido< 60 and contaminacion<5:
+        mensaje5 = "¡Buenas noticias! El clima en tu área es ideal. Disfruta, pero no olvides protegerte del sol con protector solar."
+    mensajes=[mensaje1,mensaje2,mensaje3,mensaje4,mensaje5]
+    mensajes_filtrados = [mensaje for mensaje in mensajes if len(mensaje) >= 10]
+    if mensajes_filtrados:
+        mensajeFinal = random.choice(mensajes_filtrados)
+    return mensajeFinal
+
+
+
+
+
+
+
 
 def get_temperatureCampamento(device_keyC, token,call):
     url = f'https://industrial.api.ubidots.com/api/v2.0/devices/{device_keyC}/_/values/last'
@@ -37,11 +71,10 @@ def get_temperatureCampamento(device_keyC, token,call):
         rounded_Polucion = round(Polucion_value)
         rounded_humedad = round(H_value )
         rounded_Ruido = round(Nlevel_value)
+        mensaje= analizar_variables(rounded_temperature,rounded_humedad,rounded_Ruido,rounded_Polucion)
         bot.send_message(call.message.chat.id, f"¡Gracias por compartir tu ubicación! Aquí tienes los datos climáticos para tu área")
-        bot.send_message(call.message.chat.id, f"La temperatura en Telco City es: {rounded_temperature}°C")
-        bot.send_message(call.message.chat.id, f"La Humedad en Telco City es: {rounded_humedad} %")
-        bot.send_message(call.message.chat.id, f"El nivel de polucion en Telco City es: {rounded_Polucion} ppm")
-        bot.send_message(call.message.chat.id, f"El nivel de ruido en Telco City es: {rounded_Ruido} db")
+        bot.send_message(call.message.chat.id, f"La temperatura en Telco City es: {rounded_temperature}°C\nLa Humedad en Telco City es: {rounded_humedad} %\nEl nivel de polucion en Telco City es: {rounded_Polucion} ppm\nEl nivel de ruido en Telco City es: {rounded_Ruido} db")
+        bot.send_message(call.message.chat.id, f"Consejo:\n{mensaje}")   
         markup = InlineKeyboardMarkup(row_width=2)
         item1 = InlineKeyboardButton("Sí, me gustaria saber", callback_data='temperature')
         item2 = InlineKeyboardButton("No, gracias", callback_data='later')
@@ -74,11 +107,10 @@ def get_temperatureKennedy(device_keyK,token,call):
             rounded_Polucion = round(Polucion_value)
             rounded_humedad = round(H_value )
             rounded_Ruido = round(Nlevel_value)
+            mensaje= analizar_variables(rounded_temperature,rounded_humedad,rounded_Ruido,rounded_Polucion)
             bot.send_message(call.message.chat.id, f"¡Gracias por compartir tu ubicación! Aquí tienes los datos climáticos para tu área")
-            bot.send_message(call.message.chat.id, f"La temperatura en Kennedy es: {rounded_temperature}°C")
-            bot.send_message(call.message.chat.id, f"La Humedad en Kennedy es: {rounded_humedad} %")
-            bot.send_message(call.message.chat.id, f"El nivel de polucion en Kennedy es: {rounded_Polucion} ppm")
-            bot.send_message(call.message.chat.id, f"El nivel de ruido en Kennedy es {rounded_Ruido} db")
+            bot.send_message(call.message.chat.id, f"La temperatura en Kennedy es: {rounded_temperature}°C\nLa Humedad en Kennedy es: {rounded_humedad} %\nEl nivel de polucion en Kennedy es: {rounded_Polucion} ppm\nEl nivel de ruido en Kennedy es: {rounded_Ruido} db")
+            bot.send_message(call.message.chat.id, f"Consejo:\n{mensaje}")   
             markup = InlineKeyboardMarkup(row_width=2)
             item1 = InlineKeyboardButton("Sí, me gustaria saber", callback_data='temperature')
             item2 = InlineKeyboardButton("No, gracias", callback_data='later')
@@ -192,5 +224,4 @@ if __name__ == "__main__":
 
 
 
-print('Rob $tone two damm phones babilons can`t track the code ')
-print('Dos mil 16')
+
